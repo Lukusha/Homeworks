e5ff68d398cb8f4c4ffe21cf8be19611321dd291
Lukusha <125159085+Lukusha@users.noreply.github.com>
2023-02-12 22:56:21 +0400

    Add files via upload

---
+++ Day 1/Lesson 1.py
@@ -0,0 +1,7 @@
+name = "Luka"
+surname = "Beraia"
+age = 18
+height = 6 #in ft
+weight = 74.6
+
+print("Hello! My name is " + name + " " + surname + ", I'm " + str(age) + " Years old, My height is " + str(height) + "ft and i weigh " + str(weight) + " Kg's" )
